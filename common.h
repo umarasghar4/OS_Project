a9582ae5a809e23b648b080855235ec21e32671a
Wasil Kayani <wasil8220@gmail.com>
2026-05-03 17:08:48 +0500

    Updated common.h: Added structs for ingestor/processor/reporter working

--- common.h
+++ common.h
@@ -7,11 +7,23 @@
 #define MAX_SENSORS 50        // Max unique IoT devices we expect
 #define MAX_SENSOR_NAME 32    // Max characters in a sensor's name (e.g., "Temp_LivingRoom")
 
-// Every piece of data pushed into the pipe MUST start with this.
 typedef struct {
     int chunk_id;
-    size_t byte_count;     
+    size_t byte_count;    
     int source_file_id;
 } ChunkHeader;
+
+
+typedef struct {
+    char sensor_id[MAX_SENSOR_NAME];
+    double moving_average;
+    int anomaly_count;
+} IoTAggregationRecord;
+
+
+typedef struct {
+    int unique_sensor_count;                     // How many sensors we actually found
+    IoTAggregationRecord records[MAX_SENSORS]; 
+} SharedData;
 
 #endif
